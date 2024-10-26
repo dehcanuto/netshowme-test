f979a22b67df7584d9d80f0dddab86e2bdf54f1e
André Canuto <andrecanutoc@gmail.com>
2024-10-26 12:56:22 -0300

    feat: banner home done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { SlideBanner } from "@/components/organisms";
+import bannerItems from "@/misc/bannerItems";
+
 export default function Home() {
   return (
     <main>
+      <SlideBanner slides={bannerItems} />
 
     </main>
   );
---
+++ src/misc/bannerItems.ts
@@ -0,0 +1,24 @@
+import { SlideItem } from "@/components/organisms/SlideBanner/type";
+
+const bannerItems: SlideItem[] = [
+    {
+        category: 'Over the Cast',
+        title: 'TikTok como inovação na era digital, com Rafael Kiso',
+        description: 'Os principais desafios na priorização no desenvolvimento de novos produtos.',
+        path: ''
+    },
+    {
+        category: 'Over the Cast',
+        title: 'TikTok como inovação na era digital, com Rafael Kiso',
+        description: 'Os principais desafios na priorização no desenvolvimento de novos produtos.',
+        path: ''
+    },
+    {
+        category: 'Over the Cast',
+        title: 'TikTok como inovação na era digital, com Rafael Kiso',
+        description: 'Os principais desafios na priorização no desenvolvimento de novos produtos.',
+        path: ''
+    }
+]
+
+export default bannerItems;
